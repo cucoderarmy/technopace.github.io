1b85ac8ad62e488288ed7a8442cdc45653a793cf
cucoderarmy <52543743+cucoderarmy@users.noreply.github.com>
2019-07-20 18:25:04 +0530

    Add files via upload

---
+++ static/js/custom.js
@@ -0,0 +1,301 @@
+/*========================================================================
+ Name   : Astute Resolutions
+========================================================================*/
+  
+
+(function($){
+    "use strict"
+    var WAY = {};
+
+    /*--------------------
+      * Pre Load
+    ----------------------*/
+    WAY.WebLoad = function(){
+      document.getElementById("loading").style.display = "none"; 
+    }
+
+    /*--------------------
+        * Header Class
+    ----------------------*/
+    WAY.HeaderSticky = function(){
+        $(".navbar-toggler").on("click", function(a) {
+            a.preventDefault(), 
+            $(".navbar").addClass("fixed-header")
+        });
+    }
+
+    /*--------------------
+        * Menu Close
+    ----------------------*/
+    WAY.MenuClose = function(){
+      $('.navbar-nav .nav-link').on('click', function() {
+       var toggle = $('.navbar-toggler').is(':visible');
+       if (toggle) {
+         $('.navbar-collapse').collapse('hide');
+       }
+      });
+    }
+
+
+    /*--------------------
+        * Smooth Scroll
+    ----------------------*/
+    WAY.HeaderScroll = function(){
+        $('header a[href*="#"]:not([href="#"])').on('click', function() {
+          var PathName = location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') || location.hostname == this.hostname;
+            if (PathName) {
+              var target = $(this.hash);
+                  target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+                  if (target.length) {
+                    $('html,body').animate({
+                      scrollTop: target.offset().top - 65,
+                      }, 1000);
+                      return false;
+                  }
+            }
+        });
+    }
+
+    /*--------------------
+        * Header Fixed
+    ----------------------*/
+    WAY.HeaderFixed = function(){
+      var varHeaderFix = $(window).scrollTop() >= 60;
+        if (varHeaderFix) {
+           $('.navbar').addClass('fixed-header');
+        }
+        else {
+           $('.navbar').removeClass('fixed-header');
+        }
+    }    
+
+
+    /*--------------------
+    * owl Slider
+    ----------------------*/
+
+    WAY.ClientSlider = function(){
+      var testimonials_slider = $('#client-slider-single');
+        testimonials_slider.owlCarousel({
+            loop: true,
+            margin: 0,
+            nav:false,
+            dots:true,
+            responsive: {
+              0: {
+                items: 1
+              },
+              600: {
+                items: 1
+              },
+              768: {
+                items: 2
+              },
+              991: {
+                items: 3
+              },
+              1140: {
+                items: 2
+              }
+            }
+        });
+    }
+
+    WAY.WorkSlider = function(){
+      var work_slider = $('.brand-slider');
+        work_slider.owlCarousel({
+            loop: true,
+            margin: 0,
+            nav:false,
+            dots:true,
+            responsive: {
+              0: {
+                items: 2
+              },
+              600: {
+                items: 3
+              },
+              768: {
+                items: 3
+              },
+              991: {
+                items: 4
+              },
+              1140: {
+                items: 7
+              }
+            }
+        });
+    }
+
+    WAY.PopupVideo = function(){
+      $('.popup-video').magnificPopup({
+              disableOn: 700,
+              type: 'iframe',
+              mainClass: 'mfp-fade',
+              removalDelay: 160,
+              preloader: false,
+              fixedContentPos: false
+        });
+    }
+
+    WAY.LightboxGallery = function(){
+      $('.portfolio-col').magnificPopup({
+          delegate: '.lightbox-gallery',
+          type: 'image',
+          tLoading: '#%curr%',
+          mainClass: 'mfp-fade',
+          fixedContentPos: true,
+          closeBtnInside: true,
+          gallery: {
+              enabled: true,
+              navigateByImgClick: true,
+              preload: [0, 1] // Will preload 0 - before current, and 1 after the current image
+          }
+      });
+    }
+    
+
+    /*--------------------
+        * Progress Bar 
+    ----------------------*/
+    WAY.ProgressBar = function(){
+        $(".progress .progress-bar").each(function () {
+          var bottom_object = $(this).offset().top + $(this).outerHeight();
+          var bottom_window = $(window).scrollTop() + $(window).height();
+          var progressWidth = $(this).attr('aria-valuenow') + '%';
+          if(bottom_window > bottom_object) {
+            $(this).css({
+              width : progressWidth
+            });
+          }
+        });
+    }
+
+    /*--------------------
+    * Counter JS
+    ----------------------*/
+    var a = 0;
+    WAY.Counter = function(){
+      var oTop = $('.counter-box').offset().top - window.innerHeight;
+        if (a == 0 && $(window).scrollTop() > oTop) {
+          $('.count').each(function () {
+                $(this).prop('Counter',0).animate({
+                    Counter: $(this).text()
+                }, {
+                    duration: 4000,
+                    easing: 'swing',
+                    step: function (now) {
+                        $(this).text(Math.ceil(now));
+                    }
+                });
+            });
+          a = 1;
+        }
+    }
+
+    // Window on Load
+    $(window).on("load", function(){
+      WAY.WebLoad();
+    });
+
+    $(document).on("ready", function(){
+        WAY.WorkSlider(),
+        WAY.PopupVideo(),
+        WAY.ClientSlider(),
+        WAY.LightboxGallery(),
+        WAY.MenuClose(),
+        WAY.HeaderScroll(),
+        WAY.Counter(),
+        WAY.ProgressBar(),
+        WAY.HeaderSticky();
+    });
+
+    $(window).on("scroll", function(){
+        WAY.Counter(),
+        WAY.ProgressBar(),
+        WAY.HeaderFixed();
+    });
+
+})(jQuery);
+
+
+
+
+
+function initComparisons() {
+  var x, i;
+  /* Find all elements with an "overlay" class: */
+  x = document.getElementsByClassName("img-comp-overlay");
+  for (i = 0; i < x.length; i++) {
+    /* Once for each "overlay" element:
+    pass the "overlay" element as a parameter when executing the compareImages function: */
+    compareImages(x[i]);
+  }
+  function compareImages(img) {
+    var slider, img, clicked = 0, w, h;
+    /* Get the width and height of the img element */
+    w = img.offsetWidth;
+    h = img.offsetHeight;
+    /* Set the width of the img element to 50%: */
+    img.style.width = (w / 2) + "px";
+    /* Create slider: */
+    slider = document.createElement("DIV");
+    slider.setAttribute("class", "img-comp-slider");
+    /* Insert slider */
+    img.parentElement.insertBefore(slider, img);
+    /* Position the slider in the middle: */
+    slider.style.top = (h / 2) - (slider.offsetHeight / 2) + "px";
+    slider.style.left = (w / 2) - (slider.offsetWidth / 2) + "px";
+    /* Execute a function when the mouse button is pressed: */
+    slider.addEventListener("mousedown", slideReady);
+    /* And another function when the mouse button is released: */
+    window.addEventListener("mouseup", slideFinish);
+    /* Or touched (for touch screens: */
+    slider.addEventListener("touchstart", slideReady);
+     /* And released (for touch screens: */
+    window.addEventListener("touchstop", slideFinish);
+    function slideReady(e) {
+      /* Prevent any other actions that may occur when moving over the image: */
+      e.preventDefault();
+      /* The slider is now clicked and ready to move: */
+      clicked = 1;
+      /* Execute a function when the slider is moved: */
+      window.addEventListener("mousemove", slideMove);
+      window.addEventListener("touchmove", slideMove);
+    }
+    function slideFinish() {
+      /* The slider is no longer clicked: */
+      clicked = 0;
+    }
+    function slideMove(e) {
+      var pos;
+      /* If the slider is no longer clicked, exit this function: */
+      if (clicked == 0) return false;
+      /* Get the cursor's x position: */
+      pos = getCursorPos(e)
+      /* Prevent the slider from being positioned outside the image: */
+      if (pos < 0) pos = 0;
+      if (pos > w) pos = w;
+      /* Execute a function that will resize the overlay image according to the cursor: */
+      slide(pos);
+    }
+    function getCursorPos(e) {
+      var a, x = 0;
+      e = e || window.event;
+      /* Get the x positions of the image: */
+      a = img.getBoundingClientRect();
+      /* Calculate the cursor's x coordinate, relative to the image: */
+      x = e.pageX - a.left;
+      /* Consider any page scrolling: */
+      x = x - window.pageXOffset;
+      return x;
+    }
+    function slide(x) {
+      /* Resize the image: */
+      img.style.width = x + "px";
+      /* Position the slider: */
+      slider.style.left = img.offsetWidth - (slider.offsetWidth / 2) + "px";
+    }
+  }
+} 
